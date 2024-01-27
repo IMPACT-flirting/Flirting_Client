2a09c10d3de410359429543009b36489071a708f
unknown <yjs12180825@gmail.com>
2024-01-27 15:28:41 +0900

    2024.01.27 > :heavy_minus_sign: flutter_web_auth 종속성 삭제 - 윤지상

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_web_auth/flutter_web_auth.dart';
+
+class AuthApi {
+  Future<void> signIn() async {
+    const APP_DIRECT_URL = ""; // redirect URL
+
+    final url = Uri.parse("backendURL$APP_DIRECT_URL");
+    final result = await FlutterWebAuth.authenticate(
+        url: url.toString(), callbackUrlScheme: APP_DIRECT_URL);
+
+    final accessToken = Uri.parse(result).queryParameters['access-token'];
+    final refreshToken = Uri.parse(result).queryParameters['refresh-token'];
+  }
+}
